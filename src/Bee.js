c27cb159ae1b1eea7611625413be14d5bda9b6e2
Charlie <charliebailey24@gmail.com>
2022-02-28 12:00:40 -0800

    Pseudclassical style of sprint complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,29 @@
 var Bee = function() {
+  // Q: how do I instantiate bee as an instance of Grub?
+
+  // Grub.call(this); // forgot why I needed this line -- I don't
+
+  // set the constructor
+  // constructor is a function
+
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+
+  // inherit food from grub. I think this means just don't set it
+  // this.food = Object.create(bee);
+
+  // inherit eat
+  // a job property that is set to 'keep on growing'
 };
 
+// call the Grub superclass
+Bee.prototype = new Grub;
+// set the prototype
+Bee.prototype.constructor = Bee;
+
+
+
+// var redCar = new Car ("red", 10);
+
+// Location.prototype = Object.create(Car.prototype);
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+// set the prototype
+ForagerBee.prototype = new Bee();
+
+// set the constructor
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
